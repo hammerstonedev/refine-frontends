2240290d3a6841b7f6484a3b3448cdaeb4c20996
Sean Fioritto <sean@planningforaliens.com>
2020-12-27 21:47:48 -0600

    Fix aria-expanded/hidden attributes to be dynamic.

--- src/components/tailwind/selector/selector-option.vue
+++ src/components/tailwind/selector/selector-option.vue
@@ -19,7 +19,14 @@
       class="absolute inset-y-0 right-0 flex items-center pr-4"
     >
       <!-- Heroicon name: check -->
-      <svg v-if="selected" class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+      <svg
+        v-show="selected"
+        class="h-5 w-5"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        :aria-hidden="!selected"
+      >
         <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
       </svg>
     </span>
--- src/components/tailwind/selector/selector.vue
+++ src/components/tailwind/selector/selector.vue
@@ -5,7 +5,7 @@
       <button
         type="button"
         aria-haspopup="listbox"
-        aria-expanded="true"
+        :aria-expanded="open"
         aria-labelledby="listbox-label"
         class="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
         @click="toggle()"
