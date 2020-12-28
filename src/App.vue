052687b70e75992ecd66deba72f7e1abec45295a
Sean Fioritto <sean@planningforaliens.com>
2020-12-27 21:23:30 -0600

    conditions inside labels were causing a wonky bug with click events.

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,16 @@
   <query>
     <div class="p-4">
       <div class="pt-4">
-        <label>Age
-          <text-condition id="first-name" value="Bob" class="pt-2" />
-        </label>
+        <label>Name</label>
+        <text-condition id="first-name" value="Bob" class="pt-2" />
       </div>
       <div class="pt-4">
-        <label>Age
-          <numeric-condition id="age" :value="32" class="pt-2" />
-        </label>
+        <label>Age</label>
+        <numeric-condition id="age" :value="32" class="pt-2" />
       </div>
       <div class="pt-4">
-        <label>Hotness
-          <numeric-condition id="hotness" :from="7" :to="10" class="pt-2" />
-        </label>
+        <label>Hotness</label>
+        <numeric-condition id="hotness" :from="7" :to="10" class="pt-2" />
       </div>
     </div>
   </query>
