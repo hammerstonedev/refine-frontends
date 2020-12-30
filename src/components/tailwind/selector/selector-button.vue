904ed89d223b19ee28440b395c5390dd5460d338
Sean Fioritto <sean@planningforaliens.com>
2020-12-30 13:42:00 -0600

    Refactor button to its own component.

---
+++ src/components/tailwind/selector/selector-button.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+    :id="`button-${selectorId}`"
+    type="button"
+    aria-haspopup="listbox"
+    :aria-expanded="isOpen"
+    class="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+    ref="button"
+    @click.prevent="$emit('toggle')"
+    @keydown.arrow-down.stop.prevent="$emit('open')"
+  >
+    <span class="block truncate">
+      {{  display }}
+    </span>
+    <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+      <!-- Heroicon name: selector -->
+      <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+      </svg>
+    </span>
+  </button>
+</template>
+
+<script>
+ export default {
+   name: 'selector-button',
+   props: {
+     selectorId: {
+       type: String,
+       required: true,
+     },
+     isOpen: {
+       type: Boolean,
+       required: true,
+     },
+     display: {
+       type: String,
+       required: true,
+     },
+   },
+   methods: {
+     focus: function() {
+       this.$refs.button.focus();
+     },
+   },
+ }
+</script>
