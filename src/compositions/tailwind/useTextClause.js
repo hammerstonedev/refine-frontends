f437584c1a26ca5bba7d9bb13726b4073d7037e4
Sean Fioritto <sean@planningforaliens.com>
2021-01-04 15:33:25 -0600

    Add file that's needed for the previous commit.

---
+++ src/compositions/tailwind/useTextClause.js
@@ -0,0 +1,16 @@
+import { h } from '@vue/composition-api';
+import BaseTextClause from '@/components/tailwind/clauses/text/base-text-clause';
+
+export default (
+  props,
+  ClauseComponent
+) => {
+  return () => {
+    return h(BaseTextClause, {
+      props: {
+        value: props.value,
+        ClauseComponent,
+      },
+    });
+  };
+};
