c28906b2ae8946d034d9a85ed1a989a50f71bc4a
Sean Fioritto <sean@planningforaliens.com>
2021-02-05 11:51:29 -0600

    Add uid to initial blueprint since that won't be provided by server. Change api of blueprint methods to use UID of the condition so query builder won't have to keep track of and pass around the condition instance.

--- src/stores/blueprint.js
+++ src/stores/blueprint.js
@@ -1,8 +1,25 @@
-let uid = 1;
+const getNextUid = (function() {
+  let uid = 0;
+  return () => {
+    uid = uid + 1;
+    return uid;
+  };
+})();
 
 class Blueprint {
   constructor(initialBlueprint, onChange) {
-    this.conditions = initialBlueprint || [];
+    initialBlueprint = initialBlueprint || [];
+
+    this.conditions = initialBlueprint.map((condition) => {
+      const uid = getNextUid();
+      return [
+        uid, {
+          ...condition,
+          uid: getNextUid(),
+        }
+      ];
+    });
+
     this.blueprintChanged = () => {
       console.log(JSON.parse(JSON.stringify(this.conditions)));
       if (onChange) {
@@ -11,20 +28,33 @@ class Blueprint {
     };
   }
 
-  replaceCondition(previousCondition, { id, type, depth }) {
-    depth = depth || 0;
-    const previousIndex = this.conditions.indexOf(previousCondition);
-    const newCondition = this.generateCondition({ id, type, depth });
+  indexOfCondition({ uid }) {
+    let index = -1;
+    for (let i = 0; i < this.conditions.length; i++) {
+      if (this.conditions[i].uid === uid) {
+        index = i;
+        break;
+      }
+    }
+    return index;
+  }
+
+  replaceCondition(previousCondition, nextCondition) {
+    const previousIndex = this.indexOfCondition(previousCondition);
+    const newCondition = this.generateCondition(nextCondition);
     this.conditions.splice(previousIndex, 1, newCondition);
+    this.blueprintChanged();
   }
 
   removeCondition(condition) {
-    const conditionIndex = this.conditions.indexOf(condition);
+    const conditionIndex = this.indexOfCondition(condition);
     this.conditions.splice(conditionIndex, 1);
     this.blueprintChanged();
   }
 
   generateCondition({ id, type, depth }) {
+    const uid = getNextUid();
+
     const condition = {
       id,
       type,
@@ -32,8 +62,12 @@ class Blueprint {
       input: {},
       uid,
     };
-    uid = uid + 1;
     return condition;
+  }
+
+  findCondition(uid) {
+    const conditionIndex = this.indexOfCondition({ uid });
+    return this.conditions[conditionIndex];
   }
 
   addCondition({ id, type, depth }) {
@@ -43,11 +77,12 @@ class Blueprint {
     return condition;
   }
 
-  updateInput(condition, updates) {
+  updateInput({ uid }, updates) {
     // Do the update iteratively on the input object to preserve it
     // as an observable to anything that references it. Swapping it out
     // means you can't pass it directly to anything you would always have
     // to reference condition.input everywhere versus just passing input.
+    const condition = this.findCondition(uid);
     Object.keys(updates).forEach((key) => {
       condition.input[key] = updates[key];
     });
