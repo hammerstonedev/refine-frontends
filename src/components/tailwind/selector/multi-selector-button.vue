6366d07e1c5145015ca8267d2ba906e826780080
Sean Fioritto <sean@planningforaliens.com>
2021-09-19 19:19:51 -0500

    Add an x to allow click to remove option.

--- src/components/tailwind/selector/multi-selector-button.vue
+++ src/components/tailwind/selector/multi-selector-button.vue
@@ -19,10 +19,15 @@
     <span 
       v-else
       v-for="{ id, display } in selectedOptions"
-      class="block truncate"
+      class="inline-flex p-1 border border-gray-400 rounded mr-1"
       :key="id"
     >
-    {{ display }}
+      {{ display }}
+      <span class="text-gray-500 ml-1 flex items-center cursor-pointer">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+        </svg>
+      </span>
     </span>
     <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
       <!-- Heroicon name: selector -->
