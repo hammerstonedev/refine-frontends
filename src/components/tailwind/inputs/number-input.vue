b5c64cc5e9f7b565d93cb2b93e2862d6ea974255
Aaron Francis <aarondfrancis@gmail.com>
2021-12-13 10:40:22 -0600

    Number can accept min, max, and step

--- src/components/tailwind/inputs/number-input.vue
+++ src/components/tailwind/inputs/number-input.vue
@@ -4,6 +4,7 @@
     type="number"
     :value="currentValue"
     @input="handleInputChange"
+    v-bind='metaAttributes'
   />
 </template>
 
@@ -13,30 +14,49 @@
    data() {
      return {
        currentValue: this.value,
-     };
-   },
-   methods: {
-     handleInputChange: function(event) {
-       const inputValue = event.target.value;
-       const newValue = Number(inputValue);
-
-       if (isNaN(newValue)) {
-         // don't emit. Only update with valid inputs so
-         // the blueprint won't be updated with garbage and
-         // we won't throw proptype errors
-         this.currentValue = inputValue;
-       } else {
-         this.currentValue = newValue;
-         this.$emit('input', { value: newValue })
-       }
-
-     },
+     }
    },
    props: {
      value: {
        type: Number,
        required: false,
      },
+     meta: {
+       type: Object,
+       required: false,
+       default: () => {
+         return {}
+       },
+     },
    },
- };
+   computed: {
+     metaAttributes() {
+       // A set of allowable attributes that can be passed
+       // in from the outside, usually the backend.
+       return ['min', 'max', 'step'].reduce((carry, prop) => {
+         if (Object.prototype.hasOwnProperty.call(this.meta, prop)) {
+           carry[prop] = this.meta[prop];
+         }
+
+         return carry;
+       }, {})
+     }
+   },
+   methods: {
+     handleInputChange: function (event) {
+       const inputValue = event.target.value;
+       const newValue = Number(inputValue);
+
+       if (isNaN(newValue)) {
+         // Don't emit. Only update with valid inputs so the
+         // blueprint won't be updated with garbage and
+         // we won't throw proptype errors.
+         this.currentValue = inputValue;
+       } else {
+         this.currentValue = newValue;
+         this.$emit('input', {value: newValue})
+       }
+     },
+   },
+ }
 </script>
