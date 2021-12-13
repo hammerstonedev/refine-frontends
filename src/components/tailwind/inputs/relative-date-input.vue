d609901309a43ddc13cced73619d3f9106498e71
Aaron Francis <aarondfrancis@gmail.com>
2021-12-13 15:46:39 -0600

    Relative date allows modifier and units to be passed in.

--- src/components/tailwind/inputs/relative-date-input.vue
+++ src/components/tailwind/inputs/relative-date-input.vue
@@ -1,59 +1,90 @@
 <template>
-  <div class="flex-none sm:items-center sm:flex">
+  <div class="sm:items-center sm:flex gap-x-4">
     <div>
-    <input
-      class="text-input"
-      type="number"
-      name="days"
-      :value="days"
-      @input="updateDays"
-    />
-    </div>
-    <p class="py-3 sm:px-3 sm:py-0 sm:self-center">days</p>
-    <selector @select-option="updateModifer" class="py-0">
-      <selector-option
-        id="ago"
-        display="ago"
+      <input
+        class="text-input"
+        type="number"
+        name="days"
+        :value="amount"
+        @input="updateAmount"
       />
-      <selector-option
-        id="from_now"
-        display="from now"
+    </div>
+
+    <selector @select-option="updateUnit">
+      <selector-option v-for='unit in units'
+                       :key='unit.id'
+                       :id="id('unit-' + unit.id)"
+                       :display="unit.display"
+      />
+    </selector>
+
+    <selector @select-option="updateModifier">
+      <selector-option v-for='modifier in modifiers'
+                       :key='modifier.id'
+                       :id="id('modifier-' + modifier.id)"
+                       :display="modifier.display"
       />
     </selector>
   </div>
 </template>
 
 <script>
-import { Selector, SelectorOption } from "..";
+  import {Selector, SelectorOption} from "..";
+  import {uid} from '../../../mixins';
 
-export default {
-  components: {
-    Selector,
-    SelectorOption,
-  },
-  created() {
-    const { modifier } = this;
-    this.$emit('input', { modifier });
-  },
-  methods: {
-    updateModifer: function (nextOption) {
-      this.$emit('input', { modifier: nextOption.id })
+  export default {
+    components: {
+      Selector,
+      SelectorOption,
     },
-    updateDays: function (event) {
-      const days = event.target.value;
-      this.$emit('input', { days });
+    mixins: [uid],
+    props: {
+      amount: {
+        type: [String, Number],
+        required: false,
+      },
+      unit: {
+        type: String,
+        required: false,
+      },
+      modifier: {
+        type: String,
+        required: false,
+      },
+      meta: {
+        type: Object,
+        required: false,
+        default() {
+          return {}
+        }
+      }
     },
-  },
-  props: {
-    modifier: {
-      type: String,
-      required: false,
-      default: "ago",
+    created() {
+      const {modifier} = this;
+      this.$emit('input', {modifier});
     },
-    days: {
-      type: String,
-      required: false,
+    computed: {
+      units() {
+        return this.meta.units;
+      },
+      modifiers() {
+        return this.meta.modifiers;
+      }
     },
-  },
-};
+    methods: {
+      id(seed) {
+        return `${this.uid}-${seed}`;
+      },
+      updateModifier: function (nextOption) {
+        this.$emit('input', {modifier: nextOption.id})
+      },
+      updateAmount: function (event) {
+        const amount = event.target.value;
+        this.$emit('input', {amount});
+      },
+      updateUnit: function (nextOption) {
+        this.$emit('input', {unit: nextOption.id})
+      },
+    },
+  };
 </script>
