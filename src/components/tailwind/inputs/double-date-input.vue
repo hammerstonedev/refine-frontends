4ce563d6fc80ebc87021d6544b30aa4feeb15236
Aaron Francis <aarondfrancis@gmail.com>
2021-12-18 14:26:45 -0600

    Fix class

--- src/components/tailwind/inputs/double-date-input.vue
+++ src/components/tailwind/inputs/double-date-input.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="refine-double-date-wrapper">
     <date-picker :date="date1" @input="updateFirstDate" />
-    <p class='refine-double-number-joiner' v-if='joinWord'>{{ joinWord }}</p>
+    <p class='refine-double-date-joiner' v-if='joinWord'>{{ joinWord }}</p>
     <date-picker :date="date2" @input="updateSecondDate" />
   </div>
 </template>
