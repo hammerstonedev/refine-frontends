38df671d45e44d8201e5354c74a414f8b5f3eec9
Aaron Francis <aarondfrancis@gmail.com>
2021-12-19 13:45:00 -0600

    Drop static mode components until we're ready to fully support them.

--- src/components/tailwind/index.js
+++ src/components/tailwind/index.js
@@ -1,18 +1,12 @@
 import Selector, { SelectorOption } from './selector';
-import { TextConditionOption, NumericConditionOption } from './condition-options';
-import { TextCondition, NumericCondition } from './conditions';
 import ConditionSelector from './condition-selector';
 import Query from '@/components/renderless/query';
 import QueryBuilder from '@/components/tailwind/query-builder';
 
 export {
   ConditionSelector,
-  NumericCondition,
-  TextCondition,
   Query,
   QueryBuilder,
   Selector,
   SelectorOption,
-  TextConditionOption,
-  NumericConditionOption,
 };
