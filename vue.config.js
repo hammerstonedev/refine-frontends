84554113b9c9644a48653a6b16940783be17187f
Sean Fioritto <sean@planningforaliens.com>
2021-12-19 20:15:50 -0600

    Only set externals for webpack if in production mode.

--- vue.config.js
+++ vue.config.js
@@ -4,40 +4,45 @@ module.exports = {
   css: {
     extract: true
   },
-  configureWebpack: {
-    output: {
-      libraryExport: 'default'
-    },
-    externals: [
-      webpackDateFnsExternals(), 
-      {
-        "@vue/composition-api": {
-          commonjs: "@vue/composition-api",
-          commonjs2: "@vue/composition-api",
-          amd: "@vue/composition-api",
-        }
-      }, { 
-        "vue2-datepicker": {
-          commonjs: "vue2-datepicker",
-          commonjs2: "vue2-datepicker",
-          amd: "vue2-datepicker",
-        }
-      }, {
-        "core-js": {
-          commonjs: "core-js",
-          commonjs2: "core-js",
-          amd: "core-js",
-        }
-      }, {  
-        "vue": {
-          commonjs: "vue",
-          commonjs2: "vue",
-          amd: "vue",
-        }
+  configureWebpack: () => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        output: {
+          libraryExport: 'default'
+        },
+        optimization: {
+          concatenateModules: true,
+        },
+        externals: [
+          webpackDateFnsExternals(), 
+          {
+            "@vue/composition-api": {
+              commonjs: "@vue/composition-api",
+              commonjs2: "@vue/composition-api",
+              amd: "@vue/composition-api",
+            }
+          }, { 
+            "vue2-datepicker": {
+              commonjs: "vue2-datepicker",
+              commonjs2: "vue2-datepicker",
+              amd: "vue2-datepicker",
+            }
+          }, {
+            "core-js": {
+              commonjs: "core-js",
+              commonjs2: "core-js",
+              amd: "core-js",
+            }
+          }, {  
+            "vue": {
+              commonjs: "vue",
+              commonjs2: "vue",
+              amd: "vue",
+            }
+          }
+        ],
       }
-    ],
-    optimization: {
-      concatenateModules: true,
-    },
+    } 
+    return {};
   }
 };
