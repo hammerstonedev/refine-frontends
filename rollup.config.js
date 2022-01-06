eb6fc7c60c98722c7010f1fcb6e46ca6bcd4ebd7
Sean Fioritto <sean@planningforaliens.com>
2022-01-05 22:49:00 -0600

    Simplify rollup use of babel.

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,8 @@ import alias from '@rollup/plugin-alias';
 import vue from 'rollup-plugin-vue2';
 import resolve from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
-import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
-import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import babel from '@rollup/plugin-babel';
 
 module.exports = {
   input: 'src/package.js',
@@ -15,19 +14,18 @@ module.exports = {
   ],
   output: [
     {
-        file: pkg.module,
-        format: 'es',
-        exports: "named",
-        name: 'refine-vue2',
-        sourcemap: true,
-        plugins: [getBabelOutputPlugin({ 
-          presets: [['@babel/env', { modules: 'umd' }]] 
-        })]
+      file: pkg.module,
+      format: 'es',
+      exports: "named",
+      name: 'refine-vue2',
+      sourcemap: true,
     },
   ],
   plugins: [
-    commonjs(),
     vue(),
+    babel({
+      babelHelpers: "bundled",
+    }),
     postcss({
       extensions: [ '.css' ],
       extract: true,
