cb4cfc24970b65420c5eefce15844cf472a404da
Sean Fioritto <sean@planningforaliens.com>
2022-01-10 08:34:07 -0600

    Cleanup App.uve

--- examples/vue2/src/App.vue
+++ examples/vue2/src/App.vue
@@ -9,43 +9,6 @@
       />
     </div>
     <div>{{ groupedBlueprint }}</div>
-    <!-- 
-    <query>
-      <div class="p-4">
-        <condition-selector>
-          <text-condition-option id="last-name" display="Last Name" />
-          <numeric-condition-option
-            id="years-of-experience"
-            display="Years of Experience"
-            :selected="true"
-          >
-            <is-between-option :selected="true" />
-            <is-not-between-option />
-          </numeric-condition-option>
-        </condition-selector>
-        <condition-selector>
-          <numeric-condition-option
-            id="years-of-experience"
-            display="Years of Experience"
-            :selected="true"
-          />
-          <text-condition-option id="last-name" display="Last Name" />
-        </condition-selector>
-        <div class="pt-4">
-          <label>Name</label>
-          <text-condition id="first-name" value="Bob" class="pt-2" />
-        </div>
-        <div class="pt-4">
-          <label>Age</label>
-          <numeric-condition id="age" :value="32" class="pt-2" />
-        </div>
-        <div class="pt-4">
-          <label>Hotness</label>
-          <numeric-condition id="hotness" :from="7" :to="10" class="pt-2" />
-        </div>
-      </div>
-    </query>
-    -->
   </div>
 </template>
 
