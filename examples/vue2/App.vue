5b96cb04825aadd297a934f945bcb8aa61c1dc17
Sean Fioritto <sean@planningforaliens.com>
2022-01-16 17:13:52 -0600

    Stub out vue2 dev environment based on rollup.

---
+++ examples/vue2/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>hello world</p>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
---
+++ examples/vue2/rollup.config.js
@@ -0,0 +1,47 @@
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import alias from '@rollup/plugin-alias';
+import vue from 'rollup-plugin-vue2';
+import resolve from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import replace from '@rollup/plugin-replace';
+
+module.exports = {
+  input: 'src/main.js',
+  output: {
+    format: 'iife',
+    dir: 'example',
+    entryFileNames: 'App.js',
+    sourcemap: 'inline',
+    name: 'App',
+  },
+  plugins: [
+    vue(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+      'process.env.VUE_ENV': JSON.stringify('browser')
+    }),
+    postcss({
+      extensions: [ '.css' ],
+      extract: true,
+      config: {
+        path: './postcss.config.js'
+      },
+    }),
+    resolve({
+      extensions: ['.js', '.vue'],
+    }),
+    alias({
+      "@": __dirname + '/src'
+    }),
+    serve({
+      host: 'localhost',
+      port: 8080,
+      contentBase: ['./example'],
+    }),
+    livereload({
+        watch: './example/*',
+        exts: ['html', 'js', 'css', 'vue'],
+    }),
+  ],
+};
