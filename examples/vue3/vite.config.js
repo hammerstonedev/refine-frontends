1477169845c016dee9044f65a6d75aa915b38000
Sean Fioritto <sean@planningforaliens.com>
2022-01-17 12:19:21 -0600

    Don't exclude vue-demi from optimizeDeps because that was causing vite to look up the chain for vue-demi and then using the wrong one and referencing vue2 in the top node_modules directory

--- examples/vue3/vite.config.js
+++ examples/vue3/vite.config.js
@@ -3,9 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['vue-demi'],
-  },
   plugins: [vue()],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
