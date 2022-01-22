8943d81a9284e06b54ef495694933bd9b4570181
Dave Calnan <d@ve.ie>
2022-01-22 16:49:46 +0000

    Update references to lib to packages/refine-vue

--- examples/vue2/App.vue
+++ examples/vue2/App.vue
@@ -50,8 +50,8 @@
 </template>
 
 <script>
-import { QueryBuilder } from "../../lib/src/package";
-import "../../lib/dist/vue2/refine-vue.esm.css";
+import { QueryBuilder } from "../../packages/refine-vue/src/package";
+import "../../packages/refine-vue/dist/vue2/refine-vue.esm.css";
 
 const groupedBlueprint = [];
 
