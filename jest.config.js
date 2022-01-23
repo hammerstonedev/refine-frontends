110a9d034483b97923bd3c71ef6f625fb0c10c4a
Dave Calnan <d@ve.ie>
2022-01-23 16:53:44 +0000

    Remove accidentally formatted files

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
-const { defaults } = require("jest-config");
+const {defaults} = require('jest-config');
 
 module.exports = {
   testEnvironment: "jsdom",
-  transform: {
+  "transform": {
     "^[^.]+.vue$": "@vue/vue2-jest",
-    "\\.m?js$": "babel-jest",
+    "\\.m?js$": "babel-jest"
   },
-  moduleFileExtensions: [...defaults.moduleFileExtensions, "vue"],
-  moduleNameMapper: {
+  "moduleFileExtensions": [...defaults.moduleFileExtensions, 'vue'],
+  "moduleNameMapper": {
     "^@/(.*)$": "<rootDir>/src/$1",
     "\\.(css|less|sass|scss)$": "<rootDir>/config/CSSStub.js",
   },
-  setupFiles: ["<rootDir>/tests/setup.js"],
-};
+  "setupFiles": [
+    "<rootDir>/tests/setup.js",
+  ],
+}
