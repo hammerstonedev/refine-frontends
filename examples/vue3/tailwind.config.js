9bee3cadee6037c239f4c4b3f23f8b30f85abdea
Sean Fioritto <sean@planningforaliens.com>
2022-01-23 15:21:38 -0600

    Add library source code to tailwind config in examples.

--- examples/vue3/tailwind.config.js
+++ examples/vue3/tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "../../packages/refine-vue/src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
