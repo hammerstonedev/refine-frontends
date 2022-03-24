e593bcf31ebe5a29c3dff30449fb0905a66407b2
Dave Calnan <d@ve.ie>
2022-03-24 21:44:44 +0000

    Remove formatting

--- packages/refine-vue/src/components/renderless/query.vue
+++ packages/refine-vue/src/components/renderless/query.vue
@@ -1,50 +1,50 @@
 <script>
-import { BlueprintStore } from "refine-core";
-import { isVue2 } from "vue-demi";
-
-export default {
-  props: {
-    blueprint: {
-      type: Array,
-      required: false,
+  import { BlueprintStore } from 'refine-core';
+  import { isVue2 } from 'vue-demi';
+  
+  export default {
+    props: {
+      blueprint: {
+        type: Array,
+        required: false,
+      },
+      conditions: {
+        type: Array,
+        required: false,
+      },
     },
-    conditions: {
-      type: Array,
-      required: false,
+    provide() {
+      const { blueprintStore } = this;
+      return {
+        blueprint: blueprintStore,
+        builderModeActive: false,
+      }
     },
-  },
-  provide() {
-    const { blueprintStore } = this;
-    return {
-      blueprint: blueprintStore,
-      builderModeActive: false,
-    };
-  },
-  data() {
-    return {
-      blueprintStore: new BlueprintStore(
-        this.blueprint,
-        this.conditions,
-        (updatedBlueprint) => {
-          this.$emit("change", updatedBlueprint);
-        }
-      ),
-    };
-  },
-  render() {
-    const { blueprintStore: blueprint } = this;
-    // This gets around a vue3 warning about scopedSlots being
+    data() {
+      return {
+        blueprintStore: new BlueprintStore(
+            this.blueprint,
+            this.conditions,
+            (updatedBlueprint) => {
+              this.$emit('change', updatedBlueprint)
+            },
+        ),
+      };
+    },
+    render() {
+      const { blueprintStore: blueprint } = this;
+  // This gets around a vue3 warning about scopedSlots being
     // referenced but not defined. Vue3 uses $slots and vue2 uses
     // $scopedSlots so this code allows us to work with both versions
     let defaultSlot = this.$slots?.default;
     if (isVue2) {
-      defaultSlot = this.$scopedSlots?.default;
+      defaultSlot = this.$scopedSlots?.default
     }
 
-    if (defaultSlot) {
-      return defaultSlot({ blueprint });
-    }
-    return null;
-  },
-};
+      if (defaultSlot) {
+        return defaultSlot({ blueprint });
+      }
+      return null;
+    },
+  };
 </script>
