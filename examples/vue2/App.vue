af64487be48fd806f0925c28777ffbd9beee5ebb
Robin Malfait <malfait.robin@gmail.com>
2022-04-13 00:46:51 +0200

    add Tailwind flavor

--- examples/vue2/App.vue
+++ examples/vue2/App.vue
@@ -12,7 +12,19 @@
         />
       </div>
       <div class="border">
-        <h3 class="px-4 py-2 italic bg-gray-100 w-full">Blanco flavor</h3>
+        <h3 class="px-4 py-2 italic bg-gray-100 w-full">Tailwind flavor</h3>
+        <query-builder
+          :errors="errors"
+          :conditions="conditions"
+          :flavor="tailwindFlavor"
+          v-model="groupedBlueprint"
+          class="p-4 w-100"
+        />
+      </div>
+      <div class="border">
+        <h3 class="px-4 py-2 italic bg-gray-100 w-full">
+          Blanco flavor <span class="italic text-xs">(all classes removed — no styling)</span>
+        </h3>
         <query-builder
           :errors="errors"
           :conditions="conditions"
@@ -629,6 +641,10 @@ export default {
           text: {
             class: 'refine-query-builder-and-button-span',
           },
+
+          icon: {
+            class: 'h-5 w-5',
+          },
         },
       },
       addGroupButton: {
@@ -669,7 +685,9 @@ export default {
 
         // TODO: Verify if we want this
         listbox: {
-          class: 'refine-selector-listbox',
+          class: (options) => {
+            return options.isClosed ? 'refine-selector-listbox-hidden' : 'refine-selector-listbox';
+          },
           wrapper: {
             class: 'refine-selector-listbox-wrapper',
           },
@@ -749,6 +767,21 @@ export default {
               class: 'refine-double-date-joiner',
             },
           },
+          relative: {
+            wrapper: {
+              class: 'refine-relative-date-wrapper',
+            },
+          },
+          calendar: {
+            icon: {
+              class: 'refine-date-input-calendar-icon',
+            },
+          },
+          error: {
+            icon: {
+              class: 'refine-date-input-error-icon',
+            },
+          },
         },
         number: {
           class: 'refine-number-input',
@@ -766,11 +799,156 @@ export default {
         },
       },
     };
+    let tailwindFlavor = {
+      group: {
+        class: 'flex flex-col gap-4 bg-gray-100 px-4 py-8 rounded-lg border-l-4',
+        wrapper: {
+          class: 'space-y-4',
+        },
+        addCriterionButton: {
+          class: 'flex text-gray-600',
+        },
+      },
+      addGroupButton: {
+        class: 'px-2 py-1 bg-blue-500 text-white rounded',
+      },
+      criterion: {
+        wrapper: {
+          class: 'flex flex-wrap gap-x-2 gap-y-4',
+        },
+        removeCriterionButton: {
+          class:
+            'rounded-full bg-gray-200 w-10 h-10 text-gray-600 flex items-center justify-center',
+          icon: {
+            class: 'h-5 w-5',
+          },
+        },
+        errors: {
+          class:
+            'flex-1 basis-full bg-red-50 border-l-2 border-red-600 text-red-300 px-4 py-2 rounded list-disc list-inside',
+          error: {
+            class: 'text-red-600 font-semibold',
+          },
+        },
+      },
+      select: {
+        class: 'relative sm:inline-block w-60',
+        wrapper: {
+          class: 'flex items-start gap-4',
+        },
+        customOptions: {
+          class: '',
+          wrapper: {
+            class: 'w-auto pt-4 md:flex md:pt-0',
+          },
+        },
+        listbox: {
+          class: (options) => {
+            return options.isClosed
+              ? 'hidden'
+              : 'overflow-auto text-base rounded-md max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none';
+          },
+
+          wrapper: {
+            class: 'absolute z-10 w-full mt-1 bg-white rounded-md shadow-lg',
+          },
+          item: {
+            class: (options) => {
+              return `relative py-2 pl-3 cursor-pointer select-none pr-9 ${
+                options.isHighlighted ? 'text-white bg-blue-600' : 'text-gray-900'
+              }`;
+            },
+
+            text: {
+              class: (options) =>
+                `block truncate ${options.selected ? 'font-semibold' : 'font-normal'}`,
+            },
+            icon: {
+              class: 'w-5 h-5',
+              wrapper: {
+                class: (options) =>
+                  `absolute inset-y-0 right-0 flex items-center pr-4 ${
+                    !options.isHighlighted ? 'text-blue-600' : 'text-white'
+                  }`,
+              },
+            },
+          },
+        },
+        button: {
+          class:
+            'relative w-full py-2 pl-3 pr-10 text-left bg-white border border-gray-300 rounded-md shadow-sm cursor-default; focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500',
+          placeholder: {
+            class: 'block text-gray-300 truncate select-none',
+          },
+          selected: {
+            class: 'block truncate',
+          },
+          icon: {
+            class: 'w-5 h-5 text-gray-400',
+            wrapper: {
+              class: 'absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none',
+            },
+          },
+        },
+        multi: {
+          button: {
+            class: 'refine-multi-selector-button',
+            placeholder: {
+              class: 'refine-multi-selector-button-placeholder',
+            },
+            selected: {
+              class: 'refine-multi-selector-button-selected',
+            },
+            deselect: {
+              icon: {
+                class: 'refine-multi-selector-button-deselect-icon',
+                wrapper: {
+                  class: 'refine-multi-selector-button-deselect-icon-wrapper',
+                },
+              },
+            },
+            icon: {
+              class: 'refine-multi-selector-button-icon',
+              wrapper: {
+                class: 'refine-multi-selector-button-icon-wrapper',
+              },
+            },
+          },
+        },
+      },
+      inputs: {
+        date: {
+          double: {
+            wrapper: {
+              class: 'flex items-center gap-[1ch]',
+            },
+            joiner: {
+              class: '',
+            },
+          },
+        },
+        number: {
+          class: 'refine-number-input',
+          double: {
+            wrapper: {
+              class: 'flex items-center gap-[1ch]',
+            },
+            joiner: {
+              class: '',
+            },
+          },
+        },
+        text: {
+          class: 'refine-text-input',
+        },
+      },
+    };
     return {
       conditions,
       groupedBlueprint,
       defaultFlavor,
       blancoFlavor,
+      tailwindFlavor,
       errors: {
         0: [
           {
