27efa280ee252be2a4102991551f4b4df1466f9f
Aaron Francis <Aarondfrancis@gmail.com>
2022-06-03 11:55:13 -0500

    Export flavors

--- packages/refine-vue/src/package.js
+++ packages/refine-vue/src/package.js
@@ -3,4 +3,5 @@ import DatePickerPlugin from './plugins/date-picker';
 import './assets/styles/tailwind.css';
 
 export * from './components/base';
+export * from './flavors';
 export { DatePickerPlugin };
