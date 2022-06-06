d6250fd0fa66b54471826f95cd21a02a43c05ec9
Aaron Francis <Aarondfrancis@gmail.com>
2022-06-06 17:48:46 -0500

    Add meta to other date clauses

--- packages/core/src/fixtures/conditions.ts
+++ packages/core/src/fixtures/conditions.ts
@@ -232,13 +232,71 @@ export const dateCondition: Condition = {
         id: 'exct',
         display: 'Is Exactly',
         component: 'RefineRelativeDateInput',
-        meta: {},
+        meta: {
+          units: [
+            {
+              id: 'day',
+              display: 'Days',
+            },
+            {
+              id: 'week',
+              display: 'Weeks',
+            },
+            {
+              id: 'month',
+              display: 'Months',
+            },
+            {
+              id: 'year',
+              display: 'Years',
+            },
+          ],
+          modifiers: [
+            {
+              id: 'ago',
+              display: 'Ago',
+            },
+            {
+              id: 'from_now',
+              display: 'From Now',
+            },
+          ],
+        },
       },
       {
         id: 'lt',
         display: 'Is Less Than',
         component: 'RefineRelativeDateInput',
-        meta: {},
+        meta: {
+          units: [
+            {
+              id: 'day',
+              display: 'Days',
+            },
+            {
+              id: 'week',
+              display: 'Weeks',
+            },
+            {
+              id: 'month',
+              display: 'Months',
+            },
+            {
+              id: 'year',
+              display: 'Years',
+            },
+          ],
+          modifiers: [
+            {
+              id: 'ago',
+              display: 'Ago',
+            },
+            {
+              id: 'from_now',
+              display: 'From Now',
+            },
+          ],
+        },
       },
       {
         id: 'st',
