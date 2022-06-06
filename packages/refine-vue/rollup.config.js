53197cc388a0f668f9252861eaa50e18e092c779
Aaron Francis <Aarondfrancis@gmail.com>
2022-06-06 18:02:34 -0500

    VueDemi shim in build

--- packages/refine-vue/rollup.config.js
+++ packages/refine-vue/rollup.config.js
@@ -5,13 +5,24 @@ import resolve from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
 
 let vuePlugin;
+let vueDemiShim;
 if (process.env.ROLLUP_VUE_VERSION === '2') {
   vuePlugin = vue2({
-    needMap: false
+    needMap: false,
   });
+  vueDemiShim = {};
 } else {
   // don't even run vue3 build through babel
   vuePlugin = vue();
+  vueDemiShim = {
+    resolveId(id) {
+      if (id === './vue-demi-shim') {
+        // For Vue3, point the shim at the actual VueDemi
+        // library. In Vue2 we don't use it at all.
+        return { id: 'vue-demi' };
+      }
+    },
+  };
 }
 
 module.exports = {
@@ -25,6 +36,7 @@ module.exports = {
     },
   ],
   plugins: [
+    vueDemiShim,
     vuePlugin,
     postcss({
       extensions: ['.css'],
---
+++ packages/refine-vue/src/components/base/query-builder/vue-demi-shim.js
@@ -0,0 +1,1 @@
+export const resolveComponent = (component) => component;
